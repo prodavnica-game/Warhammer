a29bf6b06d295dc8aacd5bc116b4dc3124f544f6
avujnovic3820s <avujnovic3820s@raf.rs>
2023-06-04 19:26:25 +0200

    Skripte za povezivanje sa bazom

---
+++ Scripts/Read.cs
@@ -0,0 +1,41 @@
+using MySql.Data.MySqlClient;
+using UnityEngine;
+using UnityEngine.UI;
+public class Read : MonoBehaviour
+{
+    private string connectionString;
+    string query;
+    private MySqlConnection connection;
+    private MySqlCommand command;
+    private MySqlDataReader reader;
+    public Text textCanvas;
+
+    public void viewInfo()
+    {
+        textCanvas.text = "";
+        query = "SELECT * FROM users ORDER BY (win/loss) DESC";
+
+        connectionString = "Server=localhost; database = unity_database; user = root; password = ''; charset = utf8";
+
+        connection = new MySqlConnection(connectionString);
+
+        connection.Open();
+
+        command = new MySqlCommand(query, connection);
+
+        reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            textCanvas.text += "\n" + reader[0].ToString() + "       " + reader[1].ToString() + "        " + reader[2].ToString();
+        }
+        reader.Close();
+    }
+
+    public void Awake()
+    {
+        viewInfo();
+    }
+
+}
+
---
+++ Scripts/WriteUserTwo.cs
@@ -0,0 +1,65 @@
+using MySql.Data.MySqlClient;
+using System;
+using TMPro;
+using UnityEngine;
+
+public class WriteUserTwo : MonoBehaviour
+{
+    public TMP_InputField username;
+    private string connectionString;
+    private MySqlConnection MS_Connection;
+    private MySqlCommand MS_Command;
+    string query;
+    string check_user;
+    public void sendInfo()
+    {
+        try
+        {
+            connection();
+
+
+            if (username.text == "")
+            {
+                username.text = "Guest2";
+            }
+            PlayerPrefs.SetString("UserTwo", username.text.ToLower());
+
+            check_user = "SELECT COUNT(*) FROM users WHERE username = '" + username.text.ToLower() + "';";
+
+            MS_Command = new MySqlCommand(check_user, MS_Connection);
+
+            MS_Command.ExecuteScalar();
+
+            var row_number = MS_Command.ExecuteScalar();
+
+            if (Convert.ToInt32(row_number) < 1)
+            {
+
+                query = "INSERT INTO users ( username ) VALUES ('" + username.text.ToLower() + "');";
+
+
+                MS_Command = new MySqlCommand(query, MS_Connection);
+
+                MS_Command.ExecuteNonQuery();
+
+
+                MS_Connection.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message);
+        }
+    }
+    private void connection()
+    {
+        connectionString = "Server=localhost; database = unity_database; user = root; password = ''; charset = utf8";
+        MS_Connection = new MySqlConnection(connectionString);
+
+        MS_Connection.Open();
+    }
+}
